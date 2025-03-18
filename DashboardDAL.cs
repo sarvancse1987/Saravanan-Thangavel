cbe411994e4e826ad0bd4a59f2e5506da0a3ac65
Saravanan Thangavel <sarvancse1987@yahoo.com>
2025-03-18 22:58:20 +0530

    Update DashboardDAL.cs

--- DashboardDAL.cs
+++ DashboardDAL.cs
@@ -1,135 +1,90 @@
-﻿using E2MAS.DAL.Interface;
-using E2MAS.Model;
-using E2MAS.Model.Common;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace E2MAS.DAL.Implementation
 {
-    public class DashboardDAL : IDashboardDAL
-    {
-        private readonly E2MASEntities _dbContext;
-        public DashboardDAL(E2MASEntities dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public async Task<E2MASSummary> getSummary()
-        {
-            E2MASSummary summary = null;
-            try
-            {
-                E2MASSummaryStatus obj = null;
-                obj = await _dbContext.Database.SqlQuery<E2MASSummaryStatus>("[E2MAS].[API_GetIntegratedScenarioSummary]").FirstOrDefaultAsync();
-                summary = new E2MASSummary();
-                summary.statusSummary = obj;
-                List<E2MASModelSummary> objModelSummary = null;
-                objModelSummary = await _dbContext.Database.SqlQuery<E2MASModelSummary>("[E2MAS].[API_GetScenarioModelStatus] @ScenarioID",
-                     new SqlParameter { ParameterName = "@SCENARIOID", SqlDbType = SqlDbType.Int, Direction = ParameterDirection.Input, Value = 0 }).ToListAsync();
-                summary.modelSummary = objModelSummary;             
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            return summary;
-        }
-        public async Task<List<E2MASModelSummary>> getE2MASModelSummary(int scenarioId)
-        {
-            try
-            {
-                List<E2MASModelSummary> objModelSummary = null;
-                objModelSummary = await _dbContext.Database.SqlQuery<E2MASModelSummary>("[E2MAS].[API_GetScenarioModelStatus] @ScenarioID",
-                     new SqlParameter { ParameterName = "@SCENARIOID", SqlDbType = SqlDbType.Int, Direction = ParameterDirection.Input, Value = scenarioId }).ToListAsync();
-                return objModelSummary;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public async Task<List<E2MASModelSummary>> getScenarioStatus(int scenarioId)
-        {
-            try
-            {
-                List<E2MASModelSummary> objModelSummary = null;
-                objModelSummary = await _dbContext.Database.SqlQuery<E2MASModelSummary>("[E2MAS].[API_GetScenarioModelStatus] @ScenarioID",
-                     new SqlParameter { ParameterName = "@SCENARIOID", SqlDbType = SqlDbType.Int, Direction = ParameterDirection.Input, Value = scenarioId }).ToListAsync();
-
-                return objModelSummary;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public async Task<List<ScenarioLists>> getScenarioList()
-        {
-            try
-            {
-                List<ScenarioLists> scenarioList = null;
-                scenarioList = _dbContext.ScenarioDetails.AsEnumerable().Where(x => x.IsActive == true && x.ScenarioMode == E2MASConstants.INTEGRATEDMODE && x.ScenarioStatus != E2MASConstants.SCENARIOIAPPROVED).Select((w, i) => new ScenarioLists
-                {
-                    ScenarioId = w.ScenarioId,
-                    ScenarioName = w.ScenarioName
-                }).OrderByDescending(x => x.ScenarioId).ToList();
-                return scenarioList;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public async Task<EnergyBalanceReports> getEnergyBalanceReport(int scenarioId, string countryName, int year, bool IsChanged)
-        {
-            EnergyBalanceReports objReports = null;
-            List<EnergyBalanceReport> objreport = null;
-            try
-            {
-                string lcountryName = "";
-                int lyear = 0;
-                objReports = new EnergyBalanceReports();
-                if (!IsChanged)
-                {
-                    ScenarioInputDetail objInput = await _dbContext.ScenarioInputDetails.Where(x => x.ScenarioId == scenarioId).FirstOrDefaultAsync();
-                    if (objInput != null)
-                    {
-                        objReports.startYear = objInput.StartYear;
-                        objReports.endYear = objInput.EndYear;
-                    }
-                    List<EnergyBalanceCountry> country = null;
-                    country = (from cm in _dbContext.ScenarioCountryMappings
-                               join cd in _dbContext.CountryDetails on cm.CountryID equals cd.CountryId
-                               where cm.ScenarioID == scenarioId
-                               select new EnergyBalanceCountry
-                               {
-                                   countryId = cm.CountryID,
-                                   countryName = cd.CountryName
-                               }).ToList();
-                    objReports.country = country;
-                    lcountryName = country[0].countryName;
-                    lyear = objInput.StartYear;
-                }
-                else
-                {
-                    lcountryName = countryName;
-                    lyear = year;
-                }
-                objreport = await _dbContext.Database.SqlQuery<EnergyBalanceReport>("[E2MAS].[USP_PrepareIEAReport] @SCENARIOID,@COUNTRY,@YEAR",
-                            new SqlParameter { ParameterName = "@SCENARIOID", SqlDbType = SqlDbType.Int, Direction = ParameterDirection.Input, Value = scenarioId },
-                            new SqlParameter { ParameterName = "@COUNTRY", SqlDbType = SqlDbType.VarChar, Direction = ParameterDirection.Input, Value = lcountryName },
-                            new SqlParameter { ParameterName = "@YEAR", SqlDbType = SqlDbType.Int, Direction = ParameterDirection.Input, Value = lyear }).ToListAsync();
-                objReports.report = objreport;
-                return objReports;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-    }
+  "cosmosDocType": "RecordingETARule",
+  "underwriterAssociation": {
+    "includeAll": true,
+    "includeAssociation": true,
+    "underwritersIncluded": [],
+    "underwritersExcluded": [
+      -1
+    ]
+  },
+  "policyCategoryTypeAssociation": {
+    "includeAll": true,
+    "includeAssociation": true,
+    "policyCategoryTypesExcluded": [
+      -1
+    ]
+  },
+  "policyTypeCode": "MMG",
+  "policyTypeName": "Mortgage Modification Guarantee",
+  "partitionKey": "RecordingETARule",
+  "isActive": true,
+  "isStatus": false,
+  "ruleId": 1000761,
+  "ruleName": "jhhj",
+  "changeRequestorName": "kjh",
+  "changeComments": "ju",
+  "divisionId": 1,
+  "ruleStartDate": "2025-03-12T10:04:00Z",
+  "locationAssociation": {
+    "includeAll": true,
+    "includeAssociation": true,
+    "statesIncluded": [
+      {
+        "stateCode": "AK",
+        "counties": [
+          "SITKA"
+        ]
+      }
+    ],
+    "statesExcluded": [
+      {
+        "stateCode": "",
+        "counties": []
+      }
+    ]
+  },
+  "productAssociation": {
+    "includeAll": true,
+    "includeAssociation": true,
+    "productsIncluded": [],
+    "productsExcluded": [
+      -1
+    ]
+  },
+  "transactionTypeAssociation": {
+    "includeAll": true,
+    "includeAssociation": true,
+    "transactionTypesIncluded": [],
+    "transactionTypesExcluded": [
+      -1
+    ]
+  },
+  "recordingVendorTurnDays": 19,
+  "eRecordingVendorTurnDays": 4,
+  "vendor": [
+    3633262
+  ],
+  "tenant": {
+    "masterClient": 0,
+    "subClient": 0,
+    "vendor": 0,
+    "subContractor": 0,
+    "servicerGroup": -1,
+    "servicerIds": [
+      30
+    ],
+    "lineofBusinessid": [
+      3,
+      5,
+      9
+    ]
+  },
+  "audit": {
+    "isDeleted": false,
+    "createdDate": "2025-03-12T11:45:57.5721822+00:00",
+    "createdBy": "1100040548_User",
+    "lastUpdatedDate": "2025-03-12T06:05:49.6377741+00:00",
+    "lastUpdatedBy": "1100040548_User"
+  }
 }
